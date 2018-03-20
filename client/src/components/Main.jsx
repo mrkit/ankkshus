4ade6963d2ffc9cb410d8a3a893d5f3bf2bac686
panda <neptmanhall@gmail.com>
2018-03-20 11:48:53 -0400

    removed secrets

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 import Quiz from './pages/Quiz/Quiz.jsx';
 import Login from './pages/Login/Login.jsx';
 import Distillery from './pages/Distillery/Distillery.jsx';
 import Videos from './pages/Videos/Videos.jsx';
 import Brainstorm from './pages/Brainstorm/Brainstorm.jsx';
 
-const Main = () => (
-  <main className="fade">
+class Main extends Component {
+  state = {}
+  
+  componentWillMount(){
+    const bizken = localStorage['bizken'];
     
-    <Route exact path='/' render={() => (
-      <section>
-       <h1>Hello World</h1>
-      </section>
-      )}/> 
-    <Route path='/videos' component={Videos} />
-    <Route path='/luci' render={() => window.location.reload()}/>
-    <Route path='/distillery' component={Distillery} />
-    <Route path='/quiz' component={Quiz} /> 
-    <Route path='/brainstorm' component={Brainstorm} />
-    <Route path='/login' component={Login} />
-  </main>
-)
+    if(bizken){
+        axios.defaults.headers.common.authorization = `Bearer ${bizken}`;
+    } else {
+      delete axios.defaults.headers.common.authorization
+    }
+  }
+  
+  render(){
+    return (
+      <main className="fade">
+
+      <Route exact path='/' render={() => (
+        <section>
+         <h1>Hello World</h1>
+        </section>
+        )}/> 
+      <Route path='/videos' component={Videos} />
+      <Route path='/luci' render={() => window.location.reload()}/>
+      <Route path='/distillery' component={Distillery} />
+      <Route path='/quiz' component={Quiz} /> 
+      <Route path='/brainstorm' component={Brainstorm} />
+      <Route path='/login' component={Login} />
+    </main>
+    )
+  }
+}
 
 export default Main;
