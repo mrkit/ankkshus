3840513622445590b24693cf59da6f0d1a790ef3
mrkit <neptmanhall@gmail.com>
2018-04-29 17:53:50 +0000

    fixed variable error value

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,5 @@
 const db_password = require('../secrets.js').db_password;
-const dev = 'postgres://localhost/slldb2', { logging: false, operatorsAliases: false };
+// dev => 'postgres://localhost/slldb2', { logging: false, operatorsAliases: false };
 const Sequelize = require('sequelize');
 const conn = new Sequelize('postgres',  'mrkit', db_password,  { 
 	logging: false,
