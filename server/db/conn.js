e175e7f5c7c06ab0274b0b2f1a6b3b87e22d823d
panda <neptmanhall@gmail.com>
2018-04-29 13:31:43 -0400

    added sass

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,11 +1,11 @@
 const db_password = require('../secrets.js').db_password;
-
+const dev = 'postgres://localhost/slldb2', { logging: false, operatorsAliases: false };
 const Sequelize = require('sequelize');
-const conn = new Sequelize('postgres://localhost/slldb2', { logging: false, operatorsAliases: false } /*'postgres',  'mrkit', db_password,  { 
+const conn = new Sequelize('postgres',  'mrkit', db_password,  { 
 	logging: false,
 	operatorsAliases: false, 
 	dialect: 'postgres', 
 	host: 'nphdb.cyzisvadgbvt.us-east-1.rds.amazonaws.com' 
-}*/);
+});
 
 module.exports = conn;
