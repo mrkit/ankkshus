02a574c9a37999fbd81d9d3faa4646a4c8c284a3
panda <neptmanhall@gmail.com>
2019-04-24 11:26:03 -0400

    updated babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,21 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       options: {
-        presets: [ ['env', {modules: false}], 'react', 'stage-2' ]
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: [
+          // Stage 2
+          ["@babel/plugin-proposal-decorators", { "legacy": true }],
+          "@babel/plugin-proposal-function-sent",
+          "@babel/plugin-proposal-export-namespace-from",
+          "@babel/plugin-proposal-numeric-separator",
+          "@babel/plugin-proposal-throw-expressions",
+
+          // Stage 3
+          "@babel/plugin-syntax-dynamic-import",
+          "@babel/plugin-syntax-import-meta",
+          ["@babel/plugin-proposal-class-properties", { "loose": false }],
+          "@babel/plugin-proposal-json-strings"
+        ]
       }
     }, {
       test: /\.(scss|css)$/,
